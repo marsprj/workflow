7033f528c5706deac9cc9ac11dfbd438fe3edcf5
marsprj <marsprj@gmail.com>
2017-06-02 08:31:54 +0800

    添加workflow.js

---
+++ flowchart/js/workflow.js
@@ -0,0 +1,106 @@
+var g_func_type = null;
+
+$().ready(function(){
+
+	loadFunctions();
+	initMenuEvents();
+	initNodeEvents();
+	initCanvasEvent();
+
+	initGraph();
+})
+
+function initGraph(){
+
+	var input_1_1 = g_graph.createDatumNode(50, 50, 100, 50);
+	var input_1_2 = g_graph.createDatumNode(50, 150, 100, 50);
+	var output_1  = g_graph.createDatumNode(350, 100, 100, 50);
+	var func_1    = g_graph.createFuncNode(FUNCTION_TYPE.Fusion, 200, 100, 100, 50);
+	var conn_1_1  = g_graph.createEdge(input_1_1, func_1);
+	var conn_1_2  = g_graph.createEdge(input_1_2, func_1);
+	var conn_1_3  = g_graph.createEdge(func_1, output_1);
+
+	var input_2_1 = g_graph.createDatumNode(50, 300, 100, 50);
+	//var func_2    = g_graph.createFuncNode(200, 300, 100, 50);
+	var func_2    = g_graph.createFuncNode(FUNCTION_TYPE.Stretch, 200, 300, 100, 50);
+	var output_2  = g_graph.createDatumNode(350, 300, 100, 50);
+	var conn_2_1  = g_graph.createEdge(input_2_1, func_2);
+	var conn_2_2  = g_graph.createEdge(func_2, output_2);
+
+	var func_3    = g_graph.createFuncNode(FUNCTION_TYPE.Fusion, 600, 200, 100, 50);
+	var output_3  = g_graph.createDatumNode(800, 200, 100, 50);
+	var conn_3_1  = g_graph.createEdge(output_1, func_3);
+	var conn_3_2  = g_graph.createEdge(output_2, func_3);
+	var conn_3_3  = g_graph.createEdge(func_3, output_3);
+}
+
+function initCanvasEvent(){
+	$("#canvas").dblclick(function(evt){
+		var x = evt.offsetX;
+		var y = evt.offsetY;
+		var nodeManager = NodeManager.getInstance();
+		switch(g_graph.getState()){
+			case GRAPH_STATE.ADDDATA:{
+				var node = g_graph.createDatumNode(x, y);
+			}
+			break;
+			case GRAPH_STATE.ADDFUNC:{
+				if(g_func_type){
+					var node = g_graph.createFuncNode(g_func_type, x, y);	
+				}
+				
+			}
+		}
+	})
+	// $("#canvas").mousemove(function(evt){
+	// 	console.log(evt.offsetX);
+	// })
+}
+
+function initMenuEvents(){
+	$("#add").click(function(){
+		alert("add");
+	})
+	$("#sel").click(function(){
+		g_graph.stopConnecting();
+		g_graph.draggable();
+	})
+	$("#conn").click(function(){
+		g_graph.undrag();
+		g_graph.startConnecting();
+	})
+	$("#save").click(function(){
+		//g_graph.serialize();
+		//document.getElementById("result").innerHTML = g_graph.getWorkflowText();
+		document.getElementById("result").innerHTML = g_graph.export();
+	})
+	$("#load").click(function(){
+		alert("load");
+	})
+	$("#run").click(function(){
+		alert("run");
+	})
+
+
+
+	$("#adddata").click(function(){
+		g_graph.setState(GRAPH_STATE.ADDDATA);
+	})
+	$("#addfunc").click(function(){
+		g_graph.setState(GRAPH_STATE.ADDFUNC);
+	})
+}
+
+function initNodeEvents(){
+	$(".func").click(function(){
+		g_func_type = $(this).text();
+	})
+}
+
+function loadFunctions(){
+	var html = "";
+	html += "<div class='func'>" + FUNCTION_TYPE.Stretch + "</div>";
+	html += "<div class='func'>" + FUNCTION_TYPE.Fusion + "</div>";
+
+	document.getElementById("func_container").innerHTML = html;
+}
